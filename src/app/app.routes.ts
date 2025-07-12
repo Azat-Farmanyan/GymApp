96a8958473cc30d9658055f1a69bb96479245720
Azat Farmanyan <azat.farmanyan2@mail.ru>
2025-07-12 17:01:38 +0400

    add landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'landing',
+  },
+  {
+    path: 'landing',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./components/landing/landing.component').then(
+        (c) => c.LandingComponent
+      ),
+  },
+
+  {
+    path: 'gym',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./components/gym/gym.component').then((c) => c.GymComponent),
+  },
+  {
+    path: '**',
+    redirectTo: 'landing',
+  },
+];
---
+++ src/app/components/gym/gym.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gym',
+  imports: [],
+  templateUrl: './gym.component.html',
+  styleUrl: './gym.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GymComponent { }
